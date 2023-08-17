870387dd4a093ff6d2c65f69fdc493932dca5de9
Terasuki <xt.wanglin@gmail.com>
2023-08-17 18:50:17 +0200

    create tracker

--- ranked-recorder.user.js
+++ ranked-recorder.user.js
@@ -1,33 +1,16 @@
 // ==UserScript==
-// @name         AMQ Ranked Records
+// @name         AMQ Ranked Tracker
 // @namespace    https://github.com/Terasuki
-// @version      1.0
-// @description  Tracks personal ranked results.
+// @version      1.1
+// @description  Tracks personal ranked results, current position and song distribution for Novice ranked.
 // @author       Terasuki
 // @match        https://animemusicquiz.com/*
 // @require      https://raw.githubusercontent.com/TheJoseph98/AMQ-Scripts/master/common/amqWindows.js
 // @require      https://raw.githubusercontent.com/TheJoseph98/AMQ-Scripts/master/common/amqScriptInfo.js
-// @connect      script.google.com
-// @connect      script.googleusercontent.com
-// @grant        GM_xmlhttpRequest
 // ==/UserScript==
 
 (() => {
-    
-    // Insert here Google Script App deployment link.
-    const scriptLink = 'https://script.google.com/macros/s/AKfycbygMU7pUISg-7vQ2pGIqvFSug2QdMoE-HPA3MtTKswbXbzH5wd_QUI19nwrTAL50-FwoA/exec';
 
-    // This object is sent to the spreadsheet.
-    let playerResults = {
-        name: selfName,
-        topPercent: 100,
-        pointsArray: [0],
-        opsRate: 0,
-        edsRate: 0,
-        insRate: 0
-    };
-
-    let recording = false;
     let rankedWindow;
     let rate = {
         opCorrect: 0,
@@ -37,6 +20,14 @@
         inCorrect: 0,
         ins: 0
     };
+    
+    let songsPlayed = [
+    // [OP, ED, IN] 
+        [0, 0, 0], // [60, 100]
+        [0, 0, 0], // [45, 60)
+        [0, 0, 0], // [25, 45)
+        [0, 0, 0]  // [10, 25)
+    ];
 
     // Do not load on start page.
     if (document.getElementById('startPage')) return;
@@ -52,41 +43,10 @@
     // Answer reveal.
     new Listener('answer results', (result) => {
 
-        if (quiz.gameMode !== 'Ranked') return;
+        //if (quiz.gameMode !== 'Ranked') return;
 
         updateResults(result);
-    }).bindListener();
-
-    // Game end.
-    new Listener('quiz end result', (result) => {
-
-        if (quiz.gameMode !== 'Ranked') return;
-        if (!recording) return;
-
-        playerResults.topPercent = $('#currentTop').text();
-        playerResults.opsRate = rate.ops !== 0 ? ((rate.opCorrect/rate.ops)*100).toFixed(2) : 'n/a';
-        playerResults.edsRate = rate.eds !== 0 ? ((rate.edCorrect/rate.eds)*100).toFixed(2) : 'n/a';
-        playerResults.insRate = rate.ins !== 0 ? ((rate.inCorrect/rate.ins)*100).toFixed(2) : 'n/a';
-        playerResults.name = selfName;
-
-        GM_xmlhttpRequest({
-            method: 'POST',
-            url: scriptLink,
-            headers: {
-                'Content-Type': 'application/x-www-form-urlencoded'
-            },
-            data: 'data=' + encodeURIComponent(JSON.stringify(playerResults)),
-            onload: (response) => {
-                console.log('Sent data');
-            },
-            onerror: (response) => {
-                console.log('Error');
-            } 
-        });
-
-        // Turn off script after game finishes.
-        recording = false;
-        $('#recordCheckbox').prop('checked', false);
+        updateDistribution(result);
     }).bindListener();
 
     // Before game.
@@ -104,8 +64,7 @@
                 allScores.push(player.score);
     
                 if (quiz.players[player.gamePlayerId]._name !== selfName) return;
-                    
-                playerResults.pointsArray.push(player.score);
+
                 playerPos = player.position;
                 if (result.songInfo.type === 1) {
                     rate.ops++;
@@ -134,6 +93,27 @@
         }, 1);
     }
 
+    function updateDistribution(result) {
+
+        setTimeout(() => {
+            let songDiff = result.songInfo.animeDifficulty;
+            let songType = result.songInfo.type;
+
+            let matrix_j = songType-1;
+            let matrix_i = 0;
+            if (songDiff > 60) {matrix_i = 0;}
+            else if (songDiff > 45 && songDiff <= 60) {matrix_i = 1;}
+            else if (songDiff > 25 && songDiff <= 45) {matrix_i = 2;}
+            else if (songDiff <= 25) {matrix_i = 3;}
+            songsPlayed[matrix_i][matrix_j] = songsPlayed[matrix_i][matrix_j] + 1;
+
+            $('#hard').text(`${songsPlayed[3][0]}[2] / ${songsPlayed[3][1]}[2] / ${songsPlayed[3][2]}[1]`)
+            $('#med_hard').text(`${songsPlayed[2][0]}[8] / ${songsPlayed[2][1]}[4] / ${songsPlayed[2][2]}[4]`)
+            $('#med_easy').text(`${songsPlayed[1][0]}[8] / ${songsPlayed[1][1]}[2] / ${songsPlayed[1][2]}[2]`)
+            $('#easy').text(`${songsPlayed[0][0]}[9] / ${songsPlayed[0][1]}[2] / ${songsPlayed[0][2]}[1]`)
+        }, 1)
+    }
+
     function toggleWindow(window) {
 
         if (window.isVisible()) {
@@ -146,14 +126,6 @@
 
     function resetData() {
 
-        playerResults = {
-            name: selfName,
-            topPercent: 100,
-            pointsArray: [0],
-            opsRate: 0,
-            edsRate: 0,
-            insRate: 0
-        };
         rate = {
             opCorrect: 0,
             ops: 0,
@@ -169,7 +141,7 @@
         rankedWindow = new AMQWindow({
             title: 'Ranked Stats',
             width: 300,
-            height: 350,
+            height: 370,
             draggable: true,
             zIndex: 1000,
             id: 'rankedWindow'
@@ -177,18 +149,18 @@
 
         rankedWindow.addPanel({
             width: 1.0,
-            height: 200,
+            height: 180,
             id: 'rankedResults'
         });
 
         rankedWindow.addPanel({
             width: 1.0,
-            height: 50,
+            height: 100,
             position: {
                 x: 0,
-                y: 200
+                y: 180
             },
-            id: 'rankedControl'
+            id: 'diffTracker'
         });
 
         rankedWindow.panels[0].panel.append(
@@ -224,20 +196,30 @@
         );
 
         rankedWindow.panels[1].panel.append(
-            $(`<div id='rankedControlContainer'></div>`)
+            $(`<div id='trackerContainer'></div>`)
             .append(
-                $(`<input type='checkbox' id='recordCheckbox'>`).click(() => {
-                    if ($(this).prop('checked')) {
-                        recording = false;
-                    }
-                    else {
-                        recording = true;
-                    }
-                })
+                $(
+                    `<div id='trackerLeft'>
+                        <p>Difficulties</p>
+                        <p>10-25</p>
+                        <p>25-45</p>
+                        <p>45-60</p>
+                        <p>60-100</p>
+                    </div>`
+                )
             )
             .append(
-                $(`<label for='recordCheckbox'>Record current round</label>`)
+                $(
+                    `<div id='trackerRight'>
+                        <p>OP / ED / IN</p>
+                        <p id='hard'>0</p>
+                        <p id='med_hard'>0</p>
+                        <p id='med_easy'>0</p>
+                        <p id='easy'>0</p>
+                    </div>`
+                )
             )
+            
         );
 
         let oldWidth = $('#qpOptionContainer').width();
@@ -283,13 +265,31 @@
         #rankedResultsLeft > p {
             margin-bottom: 0;
         }
+        #trackerRight {
+            padding-right: 5px;
+            text-align: right;
+            float: right;
+            width: 50%;
+        }
+        #trackerLeft {
+            padding-left: 5px;
+            text-align: left;
+            float: left;
+            width: 50%;
+        }
+        #trackerRight > p {
+            margin-bottom: 0;
+        }
+        #trackerLeft > p {
+            margin-bottom: 0;
+        }
     `);
 
     AMQ_addScriptData({
         name: 'AMQ Ranked Records',
         author: 'Terasuki',
         description: `
-            <p>Tracks current ranked position information and exports results to a designed spreadsheet.</p>
+            <p>Tracks current ranked position information and song distribution, useful for Novice runs.</p>
             <p>Thanks to TheJoseph98 for providing window code.</p>
         `
     });
